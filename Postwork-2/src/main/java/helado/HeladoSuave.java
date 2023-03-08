4f68b80e98df4dcb3eb63af9dd974d5f7af06a9e
ramiro <ceinnova13458@innovaccion.mx>
2023-03-07 22:47:04 -0600

    Clases HeladoSuave y MermeladaDecorator agregas

---
+++ Postwork-2/src/main/java/helado/HeladoSuave.java
@@ -0,0 +1,15 @@
+package com.helado;
+
+public class HeladoSuave implements Helado {
+    private final String nombreHelado = "Helado suave";
+    private int precioHelado = 30;
+    @Override
+    public String descripcion() {
+        return String.format("\n%s -- $%d", nombreHelado, precioHelado);
+    }
+
+    @Override
+    public int precio() {
+        return precioHelado;
+    }
+}
