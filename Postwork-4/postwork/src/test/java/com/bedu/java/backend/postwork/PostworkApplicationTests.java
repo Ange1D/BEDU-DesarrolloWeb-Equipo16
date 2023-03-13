238edbd9d664f2db129babbd9f18b89ff7477cad
Ange1D <47231898+Ange1D@users.noreply.github.com>
2023-03-13 17:42:51 -0600

    postwork 4 init

---
+++ Postwork-4/postwork/src/test/java/com/bedu/java/backend/postwork/PostworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bedu.java.backend.postwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
