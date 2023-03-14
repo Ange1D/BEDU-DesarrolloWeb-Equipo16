9050f8bbc9dd6cd84b54ff3130a7f4ea1a18acf1
ramiro <ceinnova13458@innovaccion.mx>
2023-03-14 16:15:18 -0600

    Clase Telefono (@Component) agregada

---
+++ Postwork-4/postwork/src/main/java/com/bedu/java/backend/postwork/components/Telefono.java
@@ -0,0 +1,35 @@
+package com.bedu.java.backend.postwork.components;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Telefono {
+    private final ValidarTelefonoService validarTelefonoService;
+    private final FormatoTelefonoService formatoTelefonoService;
+    private String telefono;
+
+    @Autowired
+    public Telefono(ValidarTelefonoService validarTelefono, FormatoTelefonoService formatoTelefonoService) {
+        this.validarTelefonoService = validarTelefono;
+        this.formatoTelefonoService = formatoTelefonoService;
+    }
+
+
+    private void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    private String getTelefono(){
+        return this.telefono;
+    }
+
+    public boolean getValidacion(String telefono) {
+        return validarTelefonoService.validarTelefono(telefono);
+    }
+
+    public String getTelefonoValidado(String telefono){
+        setTelefono(telefono);
+        return formatoTelefonoService.formatoNumero(this.getTelefono());
+    }
+}
