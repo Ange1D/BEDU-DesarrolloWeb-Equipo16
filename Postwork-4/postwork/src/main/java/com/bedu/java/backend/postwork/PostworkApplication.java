8603feff75c5caa35f4db3993adfd8f71558828c
aballesterosi <109181168+aballesterosi@users.noreply.github.com>
2023-03-14 07:14:34 -0600

    Update PostworkApplication.java
    Se implementa la interface  commandLineRunner

--- Postwork-4/postwork/src/main/java/com/bedu/java/backend/postwork/PostworkApplication.java
+++ Postwork-4/postwork/src/main/java/com/bedu/java/backend/postwork/PostworkApplication.java
@@ -1,13 +1,32 @@
-package com.bedu.java.backend.postwork;
+package org.bedu.java.backend.sesion4;
 
+import org.bedu.java.backend.sesion4.model.Persona;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.sound.midi.Soundbank;
+import java.util.Scanner;
+
 @SpringBootApplication
-public class PostworkApplication {
+public class Sesion4Application implements CommandLineRunner {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PostworkApplication.class, args);
+		SpringApplication.run(Sesion4Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Scanner reader = new Scanner(System.in);
+
+		System.out.println("Introduce el nombre: ");
+		String nonmbre = reader.nextLine();
+
+		System.out.println("Introduce el teléfono: ");
+		String telefono = reader.nextLine();
+
+		Persona persona = new Persona(nonmbre, telefono);
+		System.out.println(persona);
 	}
 
 }
