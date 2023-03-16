8ef251e2005b1f92de5249d00b41dfa51b88c859
Ange1D <47231898+Ange1D@users.noreply.github.com>
2023-03-16 15:03:03 -0600

    Postwork-6

---
+++ Postwork-6/src/main/java/org/bedu/java/backend/model/Persona.java
@@ -0,0 +1,34 @@
+package org.bedu.java.backend.model;
+
+public class Persona {
+
+    private String nombre;
+    private String telefono;
+
+
+    public Persona(String nombre, String telefono) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    @Override
+    public String toString() {
+        return "Bienvenido "+this.nombre+"\n Tu telefono es "+ this.telefono;
+    }
+}
