271f1272fa6c56eba462c93e77587ab3508c06a2
Ange1D <47231898+Ange1D@users.noreply.github.com>
2023-03-16 15:07:11 -0600

    Update PostworkApplication.java

--- Postwork-6/src/main/java/org/bedu/java/backend/PostworkApplication.java
+++ Postwork-6/src/main/java/org/bedu/java/backend/PostworkApplication.java
@@ -1,47 +1,15 @@
 package org.bedu.java.backend;
 
-import org.bedu.java.backend.model.Persona;
-import org.bedu.java.backend.service.FormatoTelefonoService;
-import org.bedu.java.backend.service.ValidadorTelefonoService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Scanner;
-
 @SpringBootApplication
-public class PostworkApplication implements CommandLineRunner {
-	private final ValidadorTelefonoService validadorTelefonoService;
-	private final FormatoTelefonoService formatoTelefonoService;
-
-	@Autowired
-	public PostworkApplication(ValidadorTelefonoService validadorTelefonoService, FormatoTelefonoService formatoTelefonoService){
-		this.validadorTelefonoService = validadorTelefonoService;
-		this.formatoTelefonoService = formatoTelefonoService;
-	}
-	public static void main(String[] args) {
-		SpringApplication.run(PostworkApplication.class, args);
-	}
+public class PostworkApplication {
 
 	@Override
 	public void run(String... args) {
-		Scanner leer = new Scanner(System.in);
 
-		System.out.print("Introduce tu nombre: ");
-		String nombre = leer.nextLine();
+        	SpringApplication.run(PostworkApplication.class, args);
 
-		System.out.print("Introduce tu número de teléfono: ");
-		String numeroTelefonico = leer.nextLine();
-
-
-		while (!validadorTelefonoService.validarTelefono(numeroTelefonico)){
-			System.out.print("Ha ocurrido un error con tu número de teléfono... introducelo nuevamente: ");
-			numeroTelefonico = leer.nextLine();
-		}
-
-		numeroTelefonico = formatoTelefonoService.formatoNumero(numeroTelefonico);
-		Persona persona = new Persona(nombre, numeroTelefonico);
-		System.out.println(persona);
 	}
 }
